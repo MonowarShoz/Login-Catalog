fcc1f26882e06315b6b5dbe02c99579b3c073770
Monowa Hossain <Monowar.shz66@gmail.com>
2021-11-09 22:24:20 +0600

    a

--- lib/widgets/constants.dart
+++ lib/widgets/constants.dart
@@ -1,37 +1,37 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' ;
 
 class UIGuide {
   //Ui Images
-  static final String grapple = "assets/ui/Grapple_login_ui.jpg";
-  static final String blackwhite = "assets/ui/Black&White_login_ui.jpg";
-  static final String lighthouse = "assets/ui/Lighthouse_login_ui.jpg";
-  static final String link = "assets/ui/Link_login_ui.jpg";
-  static final String login = "assets/ui/Login_ui.jpg";
-  static final String patterns = "assets/ui/Patterns_login_ui.jpg";
-  static final String pirple = "assets/ui/Pirple_login_ui.jpg";
-  static final String reddit = "assets/ui/Reddit_login_ui.jpg";
-  static final String background = "assets/images/background.jpg";
-  static final String logo = "assets/images/Logo.png";
-  static final String logoUn = "assets/images/monero.png";
-  // ignore: non_constant_identifier_names
-  static final String pirple_logo = "assets/images/pirple_logo.png";
-  // ignore: non_constant_identifier_names
-  static final String reddit_logo = "assets/images/reddit_logo.png";
-  static final String google = "assets/images/google.png";
+  static const String grapple = "assets/ui/Grapple_login_ui.jpg";
+  static const String blackwhite = "assets/ui/Black&White_login_ui.jpg";
+  static const String lighthouse = "assets/ui/Lighthouse_login_ui.jpg";
+  static const String link = "assets/ui/Link_login_ui.jpg";
+  static const String login = "assets/ui/Login_ui.jpg";
+  static const String patterns = "assets/ui/Patterns_login_ui.jpg";
+  static const String pirple = "assets/ui/Pirple_login_ui.jpg";
+  static const String reddit = "assets/ui/Reddit_login_ui.jpg";
+  static const String background = "assets/images/background.jpg";
+  static const String logo = "assets/images/Logo.png";
+  static const String logoUn = "assets/images/monero.png";
+  
+  static const String pirple_logo = "assets/images/pirple_logo.png";
+ 
+  static const String reddit_logo = "assets/images/reddit_logo.png";
+  static const String google = "assets/images/google.png";
 
   // Images
   // ignore: non_constant_identifier_names
-  static final String lighthouse_image = "assets/images/lighthouse.png";
+  static const String lighthouse_image = "assets/images/lighthouse.png";
   // ignore: non_constant_identifier_names
-  static final String link_image = "assets/images/link.png";
+  static const String link_image = "assets/images/link.png";
   // ignore: non_constant_identifier_names
-  static final String link_head = "assets/images/linkhead.png";
+  static const String link_head = "assets/images/linkhead.png";
   // ignore: non_constant_identifier_names
-  static final String pirple_img = "assets/images/pirple_img.jpg";
-  static final String patternimg = "assets/images/patterns.png";
+  static const String pirple_img = "assets/images/pirple_img.jpg";
+  static const String patternimg = "assets/images/patterns.png";
 
   // Videos
-  static final String backgroundvid = "assets/videos/redditvid.mp4";
+  static const String backgroundvid = "assets/videos/redditvid.mp4";
 
   // Global Colors
   static const Color WHITE = Colors.white;
